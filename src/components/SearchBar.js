61c64567e721582d01345d79b19f724307eb3bb4
okiki123 <okikimalomo17@gmail.com>
2022-01-11 11:28:12 +0100

    Initial Commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    state = {
+        searchTerm: "",
+    }
+    
+    render() {
+        return (
+            <h1>This is a SearchBar Component</h1>
+        )
+    }
+}
+
+export default SearchBar;
